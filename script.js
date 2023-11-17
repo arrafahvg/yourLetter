b480c94a8492bec20ad1c8a74f9fb302f50011ac
Mochamad Vega Arrafah Setiawan <arrafahvega@gmail.com>
2023-11-17 23:06:27 +0700

    Adding fancy countup to the letter

--- script.js
+++ script.js
@@ -8,3 +8,52 @@ function bukaSurprise() {
         alert("Wrong password, Hint: Just like the old one.");
     }
 }
+
+const startDate = new Date(2022, 9, 20, 12, 1);
+
+function getElapsedMonths(startDate, currentDate) {
+  let elapsedMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12;
+  elapsedMonths += currentDate.getMonth() - startDate.getMonth();
+  if (currentDate.getDate() < startDate.getDate()) {
+    elapsedMonths--;
+  }
+  return elapsedMonths;
+}
+
+setInterval(function() {
+  const currentDate = new Date();
+  const elapsedTime = currentDate - startDate;
+
+  const elapsedYears = Math.floor(elapsedTime / 31536000000);
+  const elapsedMonths = getElapsedMonths(startDate, currentDate) - elapsedYears * 12;
+  let elapsedDays = currentDate.getDate() - startDate.getDate(); // corrected line
+  if (elapsedDays <= 0) { // corrected line
+    elapsedDays += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(); // corrected line
+  } // corrected line
+  const elapsedHours = Math.floor(elapsedTime / 3600000) % 24;
+  const elapsedMinutes = Math.floor(elapsedTime / 60000) % 60;
+  const elapsedSeconds = Math.floor(elapsedTime / 1000) % 60;
+
+  const timeUnits = [
+    { value: elapsedYears, label: "year", pluralLabel: "years" },
+    { value: elapsedMonths, label: "month", pluralLabel: "months" },
+    { value: elapsedDays, label: "day", pluralLabel: "days" },
+    { value: elapsedHours, label: "hour", pluralLabel: "hours" },
+    { value: elapsedMinutes, label: "minute", pluralLabel: "minutes" },
+    { value: elapsedSeconds, label: "second", pluralLabel: "seconds" }
+  ];
+
+  let timerString = "";
+  let addedTimeUnits = 0;
+
+  for (const unit of timeUnits) {
+    if (unit.value > 0) {
+      timerString += `<span style="color: blue;">${unit.value} ${unit.value > 1 ? unit.pluralLabel : unit.label}</span> `;
+      addedTimeUnits++;
+    }
+  }
+
+  // Remove trailing comma and space
+
+  document.getElementById("timer").innerHTML = timerString;
+}, 1000);
