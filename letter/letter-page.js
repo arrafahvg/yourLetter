7bbee726f29ca549bf151e10b5c90b02aca61649
Mochamad Vega Arrafah Setiawan <arrafahvega@gmail.com>
2023-11-19 18:57:49 +0700

    Adjusting the bg volume

--- letter/letter-page.js
+++ letter/letter-page.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const stopBtn = document.getElementById('stopBtn');
 
   // Set default volume to 50%
-  bgMusic.volume = 0.25;
+  bgMusic.volume = 0.35;
 
   playPauseBtn.addEventListener('click', function () {
       if (bgMusic.paused) {
